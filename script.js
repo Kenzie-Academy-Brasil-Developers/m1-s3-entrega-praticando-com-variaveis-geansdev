6bcc112b771f5c50b561cd53ee59054021bb99ea
Gean <gean.96@live.com>
2022-06-14 14:29:10 -0300

    EntregaJS

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome          = "Gean"
+let sobrenome     = "Santos"
+let anoNascimento = 1996
+let cidade        = "Sapiranga"
+let anoAtual           = 2022
+
+console.log(`"Eu sou o ${nome} ${sobrenome} tenho ${anoAtual - anoNascimento} anos e moro em ${cidade}"`)
